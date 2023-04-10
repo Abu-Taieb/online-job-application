2a320beb5170e8ecadff150de5be2d6aae52f1bf
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-04-10 17:01:32 +0600

    Home Page Layout Complete

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,82 +1,77 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <>
       {/* Main Banner Section */}
       <section className=" md:flex justify-between items-center pt-9 bg-pink-50">
-        <div className="w-1/2 pr-10">
-          <h2 className="text-7xl my-4">
-            One Step <br /> Closer To Your <br /> Dream Job
+        <div className="md:w-1/2 md:pr-10 text-center md:text-left">
+          <h2 className="text-7xl my-4 text-center md:text-left p-0">
+            One Step Closer To Your Dream Job
           </h2>
-          <p className="my-8 text-2xl">
+          <p className="my-8 text-2xl p-0">
             Explore thousands of job opportunities with all the information you
             need. Its your future. Come find it. Manage all your job application
             from start to finish.
           </p>
-          <button className="btn bg-gradient-to-r from-pink-500 to-purple-500 border-0 my-4">
+          <button className=" btn bg-gradient-to-r from-pink-500 to-purple-500 border-0 my-4">
             Get Started
           </button>
         </div>
-        <div className="w-1/2">
+        <div className="md:w-1/2 mt-10">
           <img src={"chairman.png"} alt="Image" />
         </div>
       </section>
 
       {/* Job Category List  */}
       <section className="">
-        <div className="text-center py-10">
-          <h3 className="text-3xl font-bold ">Job Category List</h3>
-          <p>
+        <div className="text-center py-20">
+          <h3 className="text-4xl font-bold py-4">Job Category List</h3>
+          <p className="text-2xl py-4">
             Explore thousands of job opportunities with all the information you
             need. Its your future
           </p>
         </div>
-        <div className="md:flex">
-          <div className="card w-1/4 bg-pink-50 shadow-xl p-6 justify-start">
-            <figure>
-              <img src="/accounts 1.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title ">Account & Finance</h2>
-              <p className=" ">300 Jobs Available</p>
+        <div className="md:flex gap-10">
+          <div className="md:w-1/4 bg-pink-50 rounded-lg p-5 my-5">
+            <img className="mb-6" src="accounts1.png" alt="" />
+            <div className="text-left ">
+              <h2 className="text-2xl font-semibold">Account & Finance</h2>
+              <p className="">300 Jobs Available</p>
             </div>
           </div>
-          <div className="card w-1/4 bg-base-100 shadow-xl">
-            <figure>
-              <img src="../../assets/Icons/accounts 1.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Account & Finance</h2>
-              <p>300 Jobs Available</p>
+          <div className="md:w-1/4 bg-pink-50 rounded-lg p-5 my-5">
+            <img className="mb-6" src="accounts1.png" alt="" />
+            <div className="text-left ">
+              <h2 className="text-2xl font-semibold">Account & Finance</h2>
+              <p className="">300 Jobs Available</p>
             </div>
           </div>
-          <div className="card w-1/4 bg-base-100 shadow-xl">
-            <figure>
-              <img src="../../assets/Icons/accounts 1.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Account & Finance</h2>
-              <p>300 Jobs Available</p>
+          <div className="md:w-1/4 bg-pink-50 rounded-lg p-5 my-5">
+            <img className="mb-6" src="accounts1.png" alt="" />
+            <div className="text-left ">
+              <h2 className="text-2xl font-semibold">Account & Finance</h2>
+              <p className="">300 Jobs Available</p>
             </div>
           </div>
-          <div className="card w-1/4 bg-base-100 shadow-xl">
-            <figure>
-              <img src="../../assets/Icons/accounts 1.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Account & Finance</h2>
-              <p>300 Jobs Available</p>
+          <div className="md:w-1/4 bg-pink-50 rounded-lg p-5 my-5">
+            <img className="mb-6" src="accounts1.png" alt="" />
+            <div className="text-left ">
+              <h2 className="text-2xl font-semibold">Account & Finance</h2>
+              <p className="">300 Jobs Available</p>
             </div>
           </div>
         </div>
       </section>
 
       <section className="">
-        <div className="text-center py-10">
-          <h3 className="text-3xl font-bold ">Featured Jobs</h3>
-          <p>Explore thousands of job opportunities with all the information you
-            need. Its your future</p>
+        <div className="text-center py-20">
+          <h3 className="text-4xl font-bold py-4">Featured Jobs</h3>
+          <p className="text-2xl py-4">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
         </div>
       </section>
     </>
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className='flex py-6 bg-pink-50'>
-            <h2 className='text-3xl font-bold'><Link to="/header">Need Jobs</Link></h2>
+            <h2 className='text-3xl font-bold text-left p-0 flex items-center'><Link to="/header">Need Jobs</Link></h2>
             <nav className='flex gap-7 justify-center items-center text-2xl font-semibold'>
                 <Link to="/statistics">Statistics</Link>
                 <Link to="/appliedJobs">Applied Jobs</Link>
